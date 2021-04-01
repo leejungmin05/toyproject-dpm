649b1e82aa4212fa8c4e87c1a015ca399da42531
JungMin LEE <leejungmin05@gmail.com>
2021-04-02 02:29:32 +0900

    Add phoneBook

--- app/src/main/java/com/example/toyproject_dpm/phonebook/PhoneBookFragment.kt
+++ app/src/main/java/com/example/toyproject_dpm/phonebook/PhoneBookFragment.kt
@@ -5,17 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.toyproject_dpm.R
 import com.google.gson.Gson
 import java.io.FileNotFoundException
 
 
-class PhoneBookFragement : Fragment() {
+class PhoneBookFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
+
         }
+       // UserList.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+
+
     }
 
     override fun onCreateView(
@@ -26,18 +32,19 @@ class PhoneBookFragement : Fragment() {
         return inflater.inflate(R.layout.fragment_phone_book_fragement, container, false)
     }
 
-    fun parseJson(): List<UserDTO> {
-        val gson = Gson()
-        val testString: String
+//    fun parseJson(): List<UserDTO> {
+//        val gson = Gson()
+//        val testString: String
+//
+//        try{
+//            testString = applicationContext.assets.open("Sample_List.json").bufferedReader().use{it.readText()}
+//        }catch (e: FileNotFoundException) {
+//            e.printStackTrace()
+//            return emptyList()
+//        }
+//
+//        return gson.fromJson(testString, Array<UserDTO>::class.java).toList()
+//    }
 
-        try{
-            testString = applicationContext.assets.open("Sample_List.json").bufferedReader().use{it.readText()}
-        }catch (e: FileNotFoundException) {
-            e.printStackTrace()
-            return emptyList()
-        }
-
-        return gson.fromJson(testString, Array<UserDTO>::class.java).toList()
-    }
 
 }
