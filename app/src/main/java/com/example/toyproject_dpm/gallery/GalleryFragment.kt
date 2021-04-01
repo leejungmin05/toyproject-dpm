580bc0bbce52a9659b378ad31f46df853be77fb7
JungMin LEE <leejungmin05@gmail.com>
2021-04-02 02:30:21 +0900

    add gallery fragment

--- app/src/main/java/com/example/toyproject_dpm/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/toyproject_dpm/gallery/GalleryFragment.kt
@@ -1,10 +1,11 @@
-package com.example.toyproject_dpm
+package com.example.toyproject_dpm.gallery
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.toyproject_dpm.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Gallery.newInstance] factory method to
+ * Use the [GalleryFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Gallery : Fragment() {
+class GalleryFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -44,12 +45,12 @@ class Gallery : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Gallery.
+         * @return A new instance of fragment GalleryFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Gallery().apply {
+            GalleryFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
