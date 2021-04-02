b15548f5986e5001cbfe473b0085717a8e07f306
JungMin LEE <leejungmin05@gmail.com>
2021-04-02 23:55:17 +0900

    add PhoneBook

---
+++ app/src/main/java/com/example/toyproject_dpm/phonebook/UserAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.toyproject_dpm.phonebook
+
+import android.content.Context
+import com.example.toyproject_dpm.model.UserModel
+
+class UserAdapter(val context: Context, val items: ArrayList<UserModel>) {
+
+
+
+}
