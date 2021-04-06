c9b1e7d590f03de8b732d167ef5f5fc8aa1b7cb5
JungMin LEE <leejungmin05@gmail.com>
2021-04-07 03:20:39 +0900

    add mainActivity

--- app/src/main/java/com/example/toyproject_dpm/MainActivity.kt
+++ app/src/main/java/com/example/toyproject_dpm/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.toyproject_dpm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+        supportFragmentManager.beginTransaction().add(R.id.mainLinearLayout, PhoneBook()).commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.page_phoneBook -> {
+                supportFragmentManager.beginTransaction().replace(R.id.mainLinearLayout, PhoneBook()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.page_gallery -> {
+                supportFragmentManager.beginTransaction().replace(R.id.mainLinearLayout, Gallery()).commitAllowingStateLoss()
+                return true
+            }
+        }
+
+        return false
     }
 }
