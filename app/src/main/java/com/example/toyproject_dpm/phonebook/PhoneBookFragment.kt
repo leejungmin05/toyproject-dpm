b3c209b0a963eaa8a61f15b31938a181dc4d2a5c
JungMin LEE <leejungmin05@gmail.com>
2021-04-09 01:31:57 +0900

    add divider

--- app/src/main/java/com/example/toyproject_dpm/phonebook/PhoneBookFragment.kt
+++ app/src/main/java/com/example/toyproject_dpm/phonebook/PhoneBookFragment.kt
@@ -1,16 +1,19 @@
 package com.example.toyproject_dpm.phonebook
 
 
+import android.graphics.drawable.ClipDrawable.VERTICAL
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.toyproject_dpm.R
 import com.example.toyproject_dpm.model.UserModel
 import com.google.gson.Gson
+import kotlinx.android.synthetic.main.fragment_phone_book_fragment.*
 import kotlinx.android.synthetic.main.fragment_phone_book_fragment.view.*
 import org.json.JSONObject
 import java.io.IOException
@@ -26,6 +29,9 @@ class PhoneBookFragment : Fragment() {
     ): View? {
         val view =
             inflater.inflate(R.layout.fragment_phone_book_fragment, container, false)
+        val decoration = DividerItemDecoration(context,VERTICAL)
+        PBrcv.addItemDecoration(decoration)
+
         val userAdapter = UserAdapter()
         view.PBrcv.adapter = UserAdapter()
         view.PBrcv.layoutManager = LinearLayoutManager(context,RecyclerView.VERTICAL, false)
