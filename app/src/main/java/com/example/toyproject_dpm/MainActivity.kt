7c1dc39ab31544981527576ceb17d255acccfee1
leejungmin05 <74166576+leejungmin05@users.noreply.github.com>
2021-04-10 04:22:21 +0900

    add fab btn

--- app/src/main/java/com/example/toyproject_dpm/MainActivity.kt
+++ app/src/main/java/com/example/toyproject_dpm/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.toyproject_dpm.databinding.ActivityMainBinding
 import com.example.toyproject_dpm.gallery.GalleryFragment
 import com.example.toyproject_dpm.phonebook.PhoneBookFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_phone_book_fragment.*
 
 private const val TAG = "MainActivity"
 
@@ -30,6 +33,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         return true
     }
 
+    fun addContact(view: View) {
+        Toast.makeText(this,"clicked",Toast.LENGTH_LONG).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
@@ -37,6 +44,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         bottomNavigationView.setOnNavigationItemSelectedListener(this)
         supportFragmentManager.beginTransaction().replace(R.id.fl_container, PhoneBookFragment())
             .commit()
+
     }
 
 }
