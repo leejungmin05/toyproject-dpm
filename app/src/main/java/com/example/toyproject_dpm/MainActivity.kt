9f62b86871f5aee325871570196001252687bc95
leejungmin05 <74166576+leejungmin05@users.noreply.github.com>
2021-04-09 14:19:42 +0900

    refactor develop

--- app/src/main/java/com/example/toyproject_dpm/MainActivity.kt
+++ app/src/main/java/com/example/toyproject_dpm/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.toyproject_dpm
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.toyproject_dpm.model.UserModel
-import com.example.toyproject_dpm.phonebook.UserAdapter
-import kotlinx.android.synthetic.main.fragment_phone_book_fragment.*
+import android.util.Log
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import com.example.toyproject_dpm.databinding.ActivityMainBinding
+import com.example.toyproject_dpm.phonebook.PhoneBookFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+private const val TAG = "MainActivity"
 
 
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    private lateinit var mBinding: ActivityMainBinding
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val newFragment = when (item.itemId) {
+            R.id.page_phoneBook -> PhoneBookFragment()
+            R.id.page_gallery -> PhoneBookFragment()
+            else -> null
+        } ?: run {
+            return false
+        }
+        supportFragmentManager.beginTransaction().replace(R.id.fl_container, newFragment).commit()
+        return true
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
+        mBinding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+        supportFragmentManager.beginTransaction().replace(R.id.fl_container, PhoneBookFragment())
+            .commit()
+    }
 
 }
--- app/src/main/java/com/example/toyproject_dpm/phonebook/PhoneBookFragment.kt
+++ app/src/main/java/com/example/toyproject_dpm/phonebook/PhoneBookFragment.kt
@@ -1,52 +1,33 @@
 package com.example.toyproject_dpm.phonebook
 
 
-import android.graphics.drawable.ClipDrawable.VERTICAL
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.toyproject_dpm.R
+import com.example.toyproject_dpm.databinding.FragmentPhoneBookFragmentBinding
 import com.example.toyproject_dpm.model.UserModel
 import com.google.gson.Gson
-import kotlinx.android.synthetic.main.fragment_phone_book_fragment.*
-import kotlinx.android.synthetic.main.fragment_phone_book_fragment.view.*
 import org.json.JSONObject
 import java.io.IOException
 import java.nio.charset.Charset
 
 
-
 class PhoneBookFragment : Fragment() {
+    private lateinit var mbinding: FragmentPhoneBookFragmentBinding
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        val view =
-            inflater.inflate(R.layout.fragment_phone_book_fragment, container, false)
-        val decoration = DividerItemDecoration(context,VERTICAL)
-        PBrcv.addItemDecoration(decoration)
-
-        val userAdapter = UserAdapter()
-        view.PBrcv.adapter = UserAdapter()
-        view.PBrcv.layoutManager = LinearLayoutManager(context,RecyclerView.VERTICAL, false)
-        userAdapter.userData =  getJSONFromAssets()
-
-        return view
-    }
-
-    private fun getJSONFromAssets(): MutableList<UserModel>{
+    private fun getJSONFromAssets(): MutableList<UserModel> {
         val json: String?
         val gson = Gson()
         val userData: MutableList<UserModel> = mutableListOf()
         val charset: Charset = Charsets.UTF_8
         try {
-            val myUsersJSONFile =resources.assets.open("Sample_List.json")
+            val myUsersJSONFile = resources.assets.open("Sample_List.json")
             val size = myUsersJSONFile.available()
             val buffer = ByteArray(size)
             myUsersJSONFile.read(buffer)
@@ -56,7 +37,7 @@ class PhoneBookFragment : Fragment() {
             val jObject = JSONObject(json)
             val jArray = jObject.getJSONArray("data")
 
-            for(i in 0 until jArray.length()){
+            for (i in 0 until jArray.length()) {
                 val contactObj = gson.fromJson(jArray.get(i).toString(), UserModel::class.java)
                 userData.add(contactObj)
             }
@@ -64,6 +45,31 @@ class PhoneBookFragment : Fragment() {
             ex.printStackTrace()
         }
         return userData
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mbinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_phone_book_fragment,
+            container,
+            false
+        )
+
+        val userAdapter = UserAdapter()
+        userAdapter.userData = getJSONFromAssets()
+        mbinding.PBrcv.adapter = userAdapter
+        mbinding.PBrcv.layoutManager = LinearLayoutManager(this.context)
+        val decoration = DividerItemDecoration(
+            mbinding.PBrcv.context,
+            LinearLayoutManager(this.context).orientation
+        )
+        mbinding.PBrcv.addItemDecoration(decoration)
+
+
+        return mbinding.root
     }
 
 
