0275d95c365b88a10ad2819db01c96a8f4b3b4f1
leejungmin05 <74166576+leejungmin05@users.noreply.github.com>
2021-04-10 04:37:43 +0900

    add random Image

--- app/src/main/java/com/example/toyproject_dpm/phonebook/UserAdapter.kt
+++ app/src/main/java/com/example/toyproject_dpm/phonebook/UserAdapter.kt
@@ -42,13 +42,14 @@ class UserAdapter(
         private val userNativeTextView: TextView = view.userCountry
         private val userProfileImageView: ImageView = view.userProfile
 
+        var profileImages: IntArray = intArrayOf(R.drawable.simple0,R.drawable.simple1,R.drawable.simple2,R.drawable.simple3,R.drawable.simple4,R.drawable.simple5,R.drawable.simple6,R.drawable.simple7,R.drawable.simple8)
         fun bind(userModel: UserModel) {
             userNameTextView.text = userModel.Name
-            //ToDo ("살" string인자로 넣기)
             userAgeTextView.text = userModel.age.toString() +"살"
             userPhoneNumTextView.text = userModel.PhoneNum
             userNativeTextView.text = userModel.Country
-            userProfileImageView.setImageResource(R.drawable.simple0)
+            val random = (Math.random() * profileImages.size).toInt()
+            userProfileImageView.setImageResource(profileImages[random])
 
         }
 
